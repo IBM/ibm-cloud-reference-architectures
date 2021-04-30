83497fcdf93eeaeeed5434e379b49bbf92e71b7e
Sean Sundberg <seansund@us.ibm.com>
2021-04-30 08:53:56 -0500

    Adds scripts to create ssh keys and a workspace directory
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

---
+++ setup-workspace.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir -p workspace
+find . -type d -depth 1 | grep -vE "[.]/[.].*" | grep -v workspace | \
+  while read dir;
+do
+  name=$(echo "$dir" | sed -E "s~[.]/(.*)~\1~g")
+
+  echo "Setting up workspace/${name} from ${name}"
+
+  mkdir -p "workspace/${name}"
+  cd "workspace/${name}"
+  ln -s "../../${name}/terraform/"* .
+  ln -s ../../terraform.tfvars ./terraform.tfvars
+  ln -s ../../ssh-* .
+  cd - > /dev/null
+done
